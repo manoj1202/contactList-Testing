f1241bf50811a26c8606b38740fb94e109446254
manoj1202 <manoj.appalaneni@gmail.com>
2018-05-17 09:56:25 -0500

    Using React

--- src/App.js
+++ src/App.js
@@ -3,37 +3,6 @@ import ListContacts from './ListContacts'
 // import logo from './logo.svg';
 // import './App.css';
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           HELLO this is react sample.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-
-// class App extends Component{
-//   render(){
-//     return (
-//       <div className="App">
-//          <header className="App-header">
-//            <img src={logo} className="App-logo" alt="logo" />
-//            <h1 className="App-title">Welcome to React</h1>
-//          </header>
-//          <p className="App-intro">
-//            HELLO this is react sample.
-//          </p>      
-//       </div>
-//     )
-//   }
-// }
  
 
 
