a67dea6c56f4cc056844ae54f17a81800b4ff650
manoj1202 <manoj.appalaneni@gmail.com>
2018-05-17 11:20:37 -0500

    Using React

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -7,17 +7,19 @@ function ListContacts(props){
          <ol   className='contact-list'>
                 {props.contacts.map((contact) => (
                     <li key={contact.id} className="contact-list-item">
-                            <div className="contact-avatar" style={{
-                                backgroundImage : `URL(${contact.avatar})`
-                                  }} >
-                            </div>
-                            <div className="contact-details">
-                                <p> {contact.name}</p>
-                                <p> {contact.handle}</p>
-                            </div>
-                            <button className="contact-remove">
+                        <div className="contact-avatar" style={{
+                            backgroundImage : `URL(${contact.avatar})`
+                                }} >
+                        </div>
+                        <div className="contact-details">
+                            <p> {contact.name}</p>
+                            <p> {contact.handle}</p>
+                        </div>
+                        <button 
+                            className="contact-remove"  
+                            onclick = {() => {props.onDeleteContact(contact)}}>
                                  Remove
-                            </button>
+                        </button>
                     </li>
             
                      
