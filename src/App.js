65e40ca9831a19f57484a5e736b2e5a4897907ba
manoj1202 <manoj.appalaneni@gmail.com>
2018-05-17 09:57:10 -0500

    Using React

--- src/App.js
+++ src/App.js
@@ -53,6 +53,39 @@ import ListContacts from './ListContacts'
 //   }
 // }
 
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           HELLO this is react sample.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+// class App extends Component{
+//   render(){
+//     return (
+//       <div className="App">
+//          <header className="App-header">
+//            <img src={logo} className="App-logo" alt="logo" />
+//            <h1 className="App-title">Welcome to React</h1>
+//          </header>
+//          <p className="App-intro">
+//            HELLO this is react sample.
+//          </p>      
+//       </div>
+//     )
+//   }
+// }
+
+
 
 const contacts = [
   {
