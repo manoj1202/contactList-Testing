09e434f5037ca064e4df9221c381e2e2c113bf29
manoj1202 <manoj.appalaneni@gmail.com>
2018-05-17 13:32:53 -0500

    Using React

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -80,7 +80,9 @@ class ListContacts extends Component{
             query : query.trim()
         }))    
     }   
-
+    clearQuery = () => {
+        this.updateQuery('');
+    }
     render(){
         const {query} = this.state
         const {contacts, onDeleteContact} = this.props
@@ -100,7 +102,13 @@ class ListContacts extends Component{
                         onChange = {(event) => this.updateQuery(event.target.value)}
                     />
                 </div>
-                
+                {showingContacts.length !== contacts.length && (
+                  <div>
+                  <span> Now showing {showingcontacts.length} of {contacts.length}</span>
+                  <button onClick={this.clearQuery}>Show All</button>
+                  </div>  
+                ) 
+                }
                 <ol   className='contact-list'>
                     {showingContacts.map((contact) => (
                         <li key={contact.id} className="contact-list-item">
