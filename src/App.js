9d6a85be6bb0d8523a77b9ba88487d9448aac070
manoj1202 <manoj.appalaneni@gmail.com>
2018-05-17 09:58:35 -0500

    Using React

--- src/App.js
+++ src/App.js
@@ -87,28 +87,31 @@ import ListContacts from './ListContacts'
 
 
 
-const contacts = [
-  {
-    "id": "karen",
-    "name": "Karen Isgrigg",
-    "handle": "karen_isgrigg",
-    "avatarURL": "http://localhost:5001/karen.jpg"
-  },
-  {
-    "id": "richard",
-    "name": "Richard Kalehoff",
-    "handle": "richardkalehoff",
-    "avatarURL": "http://localhost:5001/richard.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "handle": "tylermcginnis",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
-];
+const contacts =
 
 class App extends Component {
+  state = {
+      contacts : [
+    {
+      "id": "karen",
+      "name": "Karen Isgrigg",
+      "handle": "karen_isgrigg",
+      "avatarURL": "http://localhost:5001/karen.jpg"
+    },
+    {
+      "id": "richard",
+      "name": "Richard Kalehoff",
+      "handle": "richardkalehoff",
+      "avatarURL": "http://localhost:5001/richard.jpg"
+    },
+    {
+      "id": "tyler",
+      "name": "Tyler McGinnis",
+      "handle": "tylermcginnis",
+      "avatarURL": "http://localhost:5001/tyler.jpg"
+    }
+    ];
+  }
   render(){
     return(
       <div>
