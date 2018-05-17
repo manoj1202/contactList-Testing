2f40c0041404c26aebfa665fe2c8a5a120f4530e
manoj1202 <manoj.appalaneni@gmail.com>
2018-05-17 12:22:13 -0500

    Using React

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -34,9 +34,47 @@ import PropTypes from 'prop-types'
 // }
 
 //Stateless functional components
-function ListContacts(props){
-    return(
-         <ol   className='contact-list'>
+// function ListContacts(props){
+//     return(
+//          <ol   className='contact-list'>
+//                 {props.contacts.map((contact) => (
+//                     <li key={contact.id} className="contact-list-item">
+//                         <div className="contact-avatar" style={{
+//                             backgroundImage : `URL(${contact.avatar})`
+//                                 }} >
+//                         </div>
+//                         <div className="contact-details">
+//                             <p> {contact.name}</p>
+//                             <p> {contact.handle}</p>
+//                         </div>
+//                         <button 
+//                             className="contact-remove"  
+//                             onClick = {() => props.onDeleteContact(contact)}>
+//                                  Remove
+//                         </button>
+//                     </li>
+            
+                     
+//                 ))}
+
+//             </ol>
+//     )
+// }
+
+// ListContacts.propTypes = {
+//    contacts : PropTypes.array.isRequired ,
+//     onDeleteContact : PropTypes.func.isRequired,
+// }
+
+class ListContacts extends Component{
+    static propTypes ={
+        contacts : PropTypes.array.isRequired ,
+        onDeleteContact : PropTypes.func.isRequired,
+    }
+
+    render(){
+        return(
+             <ol   className='contact-list'>
                 {props.contacts.map((contact) => (
                     <li key={contact.id} className="contact-list-item">
                         <div className="contact-avatar" style={{
@@ -58,12 +96,8 @@ function ListContacts(props){
                 ))}
 
             </ol>
-    )
-}
-
-ListContacts.propTypes = {
-   contacts : PropTypes.array.isRequired ,
-    onDeleteContact : PropTypes.func.isRequired,
+        )
+    }
 }
 
 export default ListContacts
