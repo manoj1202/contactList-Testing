59e87604ced51143f4d2d19e538a232ac685abe6
manoj1202 <manoj.appalaneni@gmail.com>
2018-05-18 09:44:09 -0500

    BrowserRouter

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,9 +1,15 @@
 import React,{Component} from 'react'
-
+import {Link} from 'react-router-dom'
 class CreateContact extends Component{
     render(){
         return(
-            <div>Create Contact</div>
+            <div>
+                <button>
+                <Link 
+                to = '/'>Back
+                </Link>
+                </button>
+            </div>
         )
     }
 }
