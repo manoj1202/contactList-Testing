ce1f288c40dcb0d804497314133221cbbcccce41
manoj1202 <manoj.appalaneni@gmail.com>
2018-05-18 11:24:41 -0500

    BrowserRouter

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,6 +1,10 @@
 import React,{Component} from 'react'
 import {Link} from 'react-router-dom'
 class CreateContact extends Component{
+    handleSubmit = (e) => {
+        e.preventDefault()
+        
+    }
     render(){
         return(
             <div>
